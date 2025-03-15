4b747567f376232abea364fa401544e2c42a4b84
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-15 10:59:49 +0545

    Config adjusted

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -12,7 +12,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 const auth = getAuth(app)
