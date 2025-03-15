eb0acea283b943c2930bdd992e46973d02cb3280
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-15 11:04:20 +0545

    Get product Data

--- src/lib/getProductData.ts
+++ src/lib/getProductData.ts
@@ -1,10 +1,9 @@
-// lib/getProductData.ts
 import { db } from '@/firebase/config'
 import { doc, getDoc } from 'firebase/firestore'
 
 export async function getProductData(slug: string) {
   try {
-    const productRef = doc(db, 'products', slug) // Ensure slug is a valid document ID
+    const productRef = doc(db, 'products', slug) 
     const docSnap = await getDoc(productRef)
     if (docSnap.exists()) {
       return { id: docSnap.id, ...docSnap.data() }
