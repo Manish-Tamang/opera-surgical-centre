763018663f4ef88c9ab54aaf8f93ca70ca63c989
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-15 10:59:27 +0545

    Hero Section Update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -94,10 +94,10 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
               Opera Surgical Centre
             </h1>
-            <p className="mt-6 text-lg text-gray-600">
+            <p className="mt-6 text-sm text-gray-600">
               we provides a comprehensive, integrated range of clinical services
               from our various sites ranging from acute tertiary services in
               areas of emergency medicine, intensive care, medical and surgical
